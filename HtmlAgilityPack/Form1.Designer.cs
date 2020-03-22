6d2ef8ffb861241b2e8a521089d65cea9fb91305
BURAK GÖMEÇ <48028551+BurakGomec@users.noreply.github.com>
2020-03-22 20:36:09 +0300

    Add files via upload

--- HtmlAgilityPack/Form1.Designer.cs
+++ HtmlAgilityPack/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
@@ -38,9 +40,9 @@
             this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button1.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(138, 246);
+            this.button1.Location = new System.Drawing.Point(19, 433);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(411, 130);
+            this.button1.Size = new System.Drawing.Size(315, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "Baslik Getir!";
             this.button1.UseVisualStyleBackColor = false;
@@ -65,12 +67,36 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Adresi Giriniz:";
             // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(574, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(315, 55);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Verileri Getir!";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(19, 138);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(870, 260);
+            this.listBox1.TabIndex = 6;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(922, 518);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -87,6 +113,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- HtmlAgilityPack/Form1.cs
+++ HtmlAgilityPack/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Net;
+using HtmlAgilityPack;
 
 namespace HtmlAgilityPack
 {
@@ -22,26 +23,71 @@ namespace HtmlAgilityPack
         }
 
         private void button1_Click(object sender, EventArgs e)
-        { 
-            string adres;
-            adres = textBox1.Text;//kullanıcının istediği adresi alıyor
-            WebRequest istek = HttpWebRequest.Create(adres);
-            WebResponse cevap;
-            cevap = istek.GetResponse();//cevabı al 
-            StreamReader gelenbilgiler = new StreamReader(cevap.GetResponseStream());
-            string gelen = gelenbilgiler.ReadToEnd(); //gelenbilgileri okumayı sonlandir
-            int baslikbaslangic = gelen.IndexOf("<title>")+7; //<title>dan sayiyi oku
-            int baslikbitis = gelen.Substring(baslikbaslangic).IndexOf("</title>");
-            string baslik = gelen.Substring(baslikbaslangic,baslikbitis);
-            MessageBox.Show(baslik);
+        {
+            try
+            {
+                string adres;
+                adres = textBox1.Text;//kullanıcının istediği adresi alıyor
+                WebRequest istek = HttpWebRequest.Create(adres);
+                WebResponse cevap;
+                cevap = istek.GetResponse();//cevabı al 
+                StreamReader gelenbilgiler = new StreamReader(cevap.GetResponseStream());
+                string gelen = gelenbilgiler.ReadToEnd(); //gelenbilgileri okumayı sonlandir
+                int baslikbaslangic = gelen.IndexOf("<title>") + 7; //<title>dan sayiyi oku
+                int baslikbitis = gelen.Substring(baslikbaslangic).IndexOf("</title>");
+                string baslik = gelen.Substring(baslikbaslangic, baslikbitis);
+                MessageBox.Show(baslik);
 
 
 
 
+            }
+            catch (FormatException)
+            {
 
+                MessageBox.Show("Giris Formatiniz Hatali,Tekrar Deneyin");
+            }
+            catch(Exception E)
+            {
+                MessageBox.Show(E.ToString());
+            }
+           
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string adres2 = textBox1.Text;
+                Uri url = new Uri(adres2);
+                WebClient client = new WebClient();
+                client.Encoding = System.Text.Encoding.UTF8; //Turkce karakter duzeltmesi
+                string html = client.DownloadString(url);
+                HtmlAgilityPack.HtmlDocument dokuman = new HtmlAgilityPack.HtmlDocument();
+                dokuman.LoadHtml(html);
+                HtmlNodeCollection basliklar = dokuman.DocumentNode.SelectNodes("//a");
 
+                foreach (var baslik in basliklar)
+                {
+                    string link = baslik.Attributes["href"].Value;
+                    listBox1.Items.Add(baslik.InnerText);
 
+                }
+
+            }
+            catch (UriFormatException)
+            {
+                MessageBox.Show("Giris Formatiniz Hatali,Tekrar Deneyin");
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Giris Formatiniz Hatali,Tekrar Deneyin");
+            }
+            
+            catch (Exception E)
+            {
+                MessageBox.Show(E.ToString());
+            }
 
 
 
